8e3ae122df00437e049c93b5adf24498083800f7
silenthill99 <florian.graziani@sfr.fr>
2023-12-16 23:10:27 +0100

    Création du model des items avec les Data Generator, pour simplifier la tache

--- src/main/java/fr/silenthill99/codelyokomod/data/models/ItemModelGenerator.java
+++ src/main/java/fr/silenthill99/codelyokomod/data/models/ItemModelGenerator.java
@@ -24,6 +24,14 @@ public class ItemModelGenerator extends ItemModelProvider {
         spawnEgg(ModItems.JEREMY_BELPOIS_SPAWN_EGG);
         spawnEgg(ModItems.JIM_MORALES_SPAWN_EGG);
         customItem(ModItems.LONGUE, GeneratedOrHandheld.GENERATED);
+        customItem(ModItems.LYOKO_GUERRIER_BOOTS, GeneratedOrHandheld.GENERATED);
+        customItem(ModItems.LYOKO_GUERRIER_CHESTPLATE, GeneratedOrHandheld.GENERATED);
+        customItem(ModItems.LYOKO_GUERRIER_HELMET, GeneratedOrHandheld.GENERATED);
+        customItem(ModItems.LYOKO_GUERRIER_LEGGINGS, GeneratedOrHandheld.GENERATED);
+        spawnEgg(ModItems.ODD_DELLA_ROBBIA_SPAWN_EGG);
+        spawnEgg(ModItems.ULRICH_STERN_SPAWN_ITEM);
+        spawnEgg(ModItems.WILLIAM_DUNBAR_SPAWN_EGG);
+        spawnEgg(ModItems.YUMI_ISHIYAMA_SPAWN_EGG);
     }
 
     private void customItem(RegistryObject<Item> item, GeneratedOrHandheld generatedOrHandheld) {

2160b0a9e58d06728b141e889edbd27518efeae2
silenthill99 <florian.graziani@sfr.fr>
2024-07-28 14:54:12 +0200

    Changement d'un détail

--- src/main/java/fr/silenthill99/codelyokomod/data/models/ItemModelGenerator.java
+++ src/main/java/fr/silenthill99/codelyokomod/data/models/ItemModelGenerator.java
@@ -8,37 +8,41 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.fml.RegistryObject;
 
+@SuppressWarnings("DataFlowIssue")
 public class ItemModelGenerator extends ItemModelProvider {
     public ItemModelGenerator(DataGenerator generator, ExistingFileHelper existingFileHelper) {
         super(generator, Main.MODID, existingFileHelper);
     }
 
+    ResourceLocation path;
+
     @Override
     protected void registerModels() {
-        spawnEgg(ModItems.AELITA_SCHAEFFER_SPAWN_EGG);
-        customItem(ModItems.COURT);
-        spawnEgg(ModItems.JEREMY_BELPOIS_SPAWN_EGG);
-        spawnEgg(ModItems.JIM_MORALES_SPAWN_EGG);
-        customItem(ModItems.LONGUE);
-        customItem(ModItems.LYOKO_GUERRIER_BOOTS);
-        customItem(ModItems.LYOKO_GUERRIER_CHESTPLATE);
-        customItem(ModItems.LYOKO_GUERRIER_HELMET);
-        customItem(ModItems.LYOKO_GUERRIER_LEGGINGS);
-        spawnEgg(ModItems.ODD_DELLA_ROBBIA_SPAWN_EGG);
-        spawnEgg(ModItems.ULRICH_STERN_SPAWN_ITEM);
-        spawnEgg(ModItems.WILLIAM_DUNBAR_SPAWN_EGG);
-        spawnEgg(ModItems.YUMI_ISHIYAMA_SPAWN_EGG);
+        spawnEgg(ModItems.AELITA_SCHAEFFER_SPAWN_EGG.get());
+        customItem(ModItems.COURT.get());
+        spawnEgg(ModItems.JEREMY_BELPOIS_SPAWN_EGG.get());
+        spawnEgg(ModItems.JIM_MORALES_SPAWN_EGG.get());
+        customItem(ModItems.LONGUE.get());
+        customItem(ModItems.LYOKO_GUERRIER_BOOTS.get());
+        customItem(ModItems.LYOKO_GUERRIER_CHESTPLATE.get());
+        customItem(ModItems.LYOKO_GUERRIER_HELMET.get());
+        customItem(ModItems.LYOKO_GUERRIER_LEGGINGS.get());
+        spawnEgg(ModItems.ODD_DELLA_ROBBIA_SPAWN_EGG.get());
+        spawnEgg(ModItems.ULRICH_STERN_SPAWN_ITEM.get());
+        spawnEgg(ModItems.WILLIAM_DUNBAR_SPAWN_EGG.get());
+        spawnEgg(ModItems.YUMI_ISHIYAMA_SPAWN_EGG.get());
     }
 
-    private void customItem(RegistryObject<Item> item) {
-        withExistingParent(item.getId().getPath(), new ResourceLocation("item/generated"))
-                .texture("layer0", new ResourceLocation(Main.MODID, "item/" + item.getId().getPath()));
+    private void customItem(Item item) {
+        path = item.getRegistryName();
+        withExistingParent(path.getPath(), new ResourceLocation("item/generated"))
+                .texture("layer0", new ResourceLocation(Main.MODID, "item/" + path.getPath()));
     }
 
-    private void spawnEgg(RegistryObject<ForgeSpawnEggItem> item) {
-        withExistingParent(item.getId().getPath(), new ResourceLocation("item/template_spawn_egg"));
+    private void spawnEgg(ForgeSpawnEggItem item) {
+        path = item.getRegistryName();
+        withExistingParent(path.getPath(), new ResourceLocation("item/template_spawn_egg"));
     }
 
 }

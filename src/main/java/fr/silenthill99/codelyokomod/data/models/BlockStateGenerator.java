2b0b873d0d5f993e2faa14815884b95acaeee74f
silenthill99 <florian.graziani@sfr.fr>
2024-07-28 13:51:36 +0200

    Ajout du mapping Parchment, finition du data generator du model des blocs et correction d'erreurs dans le data generator du model des items

--- src/main/java/fr/silenthill99/codelyokomod/data/models/BlockStateGenerator.java
+++ src/main/java/fr/silenthill99/codelyokomod/data/models/BlockStateGenerator.java
@@ -2,9 +2,11 @@ package fr.silenthill99.codelyokomod.data.models;
 
 import fr.silenthill99.codelyokomod.Main;
 import fr.silenthill99.codelyokomod.init.ModBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class BlockStateGenerator extends BlockStateProvider {
@@ -17,6 +19,18 @@ public class BlockStateGenerator extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
         simpleBlock(ModBlocks.ENTRY_BLOCK.get(), cubeAll(Blocks.BLACK_WOOL));
-        simpleBlockItem(ModBlocks.ENTRY_BLOCK.get(), cubeAll(Blocks.BLACK_WOOL));
+        horizontalBlock(ModBlocks.SUPER_CALCULATEUR.get(), new ModelFile.UncheckedModelFile(modLoc("block/supercalculateur")));
+    }
+
+    @Override
+    public void simpleBlock(Block block, ModelFile model) {
+        super.simpleBlock(block, model);
+        simpleBlockItem(block, model);
+    }
+
+    @Override
+    public void horizontalBlock(Block block, ModelFile model) {
+        super.horizontalBlock(block, model);
+        simpleBlockItem(block, model);
     }
 }
--- src/main/java/fr/silenthill99/codelyokomod/data/models/ItemModelGenerator.java
+++ src/main/java/fr/silenthill99/codelyokomod/data/models/ItemModelGenerator.java
@@ -10,8 +10,6 @@ import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.fml.RegistryObject;
 
-import java.util.Locale;
-
 public class ItemModelGenerator extends ItemModelProvider {
     public ItemModelGenerator(DataGenerator generator, ExistingFileHelper existingFileHelper) {
         super(generator, Main.MODID, existingFileHelper);
@@ -20,31 +18,27 @@ public class ItemModelGenerator extends ItemModelProvider {
     @Override
     protected void registerModels() {
         spawnEgg(ModItems.AELITA_SCHAEFFER_SPAWN_EGG);
-        customItem(ModItems.COURT, GeneratedOrHandheld.GENERATED);
+        customItem(ModItems.COURT);
         spawnEgg(ModItems.JEREMY_BELPOIS_SPAWN_EGG);
         spawnEgg(ModItems.JIM_MORALES_SPAWN_EGG);
-        customItem(ModItems.LONGUE, GeneratedOrHandheld.GENERATED);
-        customItem(ModItems.LYOKO_GUERRIER_BOOTS, GeneratedOrHandheld.GENERATED);
-        customItem(ModItems.LYOKO_GUERRIER_CHESTPLATE, GeneratedOrHandheld.GENERATED);
-        customItem(ModItems.LYOKO_GUERRIER_HELMET, GeneratedOrHandheld.GENERATED);
-        customItem(ModItems.LYOKO_GUERRIER_LEGGINGS, GeneratedOrHandheld.GENERATED);
+        customItem(ModItems.LONGUE);
+        customItem(ModItems.LYOKO_GUERRIER_BOOTS);
+        customItem(ModItems.LYOKO_GUERRIER_CHESTPLATE);
+        customItem(ModItems.LYOKO_GUERRIER_HELMET);
+        customItem(ModItems.LYOKO_GUERRIER_LEGGINGS);
         spawnEgg(ModItems.ODD_DELLA_ROBBIA_SPAWN_EGG);
         spawnEgg(ModItems.ULRICH_STERN_SPAWN_ITEM);
         spawnEgg(ModItems.WILLIAM_DUNBAR_SPAWN_EGG);
         spawnEgg(ModItems.YUMI_ISHIYAMA_SPAWN_EGG);
     }
 
-    private void customItem(RegistryObject<Item> item, GeneratedOrHandheld generatedOrHandheld) {
-        withExistingParent(item.getId().getPath(), new ResourceLocation("item/" + generatedOrHandheld.name().toLowerCase(Locale.ROOT)))
-                .texture("parent", new ResourceLocation(Main.MODID, "item/" + item.getId().getPath()));
+    private void customItem(RegistryObject<Item> item) {
+        withExistingParent(item.getId().getPath(), new ResourceLocation("item/generated"))
+                .texture("layer0", new ResourceLocation(Main.MODID, "item/" + item.getId().getPath()));
     }
 
     private void spawnEgg(RegistryObject<ForgeSpawnEggItem> item) {
         withExistingParent(item.getId().getPath(), new ResourceLocation("item/template_spawn_egg"));
     }
 
-    public enum GeneratedOrHandheld {
-        GENERATED,
-        HANDHELD
-    }
 }
